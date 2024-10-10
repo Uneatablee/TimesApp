fe1ef149e2f3f770c4dcf4b1dfdde09b4f0cd58a
Jakub <jakubpasek11@gmail.com>
2024-10-11 00:37:53 +0200

    git submodule fix 1

--- tests/unit.tests/business_logic/DayPlanner/Calendar/CalendarUnitTests.cpp
+++ tests/unit.tests/business_logic/DayPlanner/Calendar/CalendarUnitTests.cpp
@@ -1,6 +1,6 @@
 #include "catch2/catch_test_macros.hpp"
 #include "Calendar.hpp"
-#include "fakeit.hpp"
+#include "../../../../../lib/FakeIt/single_header/catch/fakeit.hpp"
 
 TEST_CASE("Calendar can return current name")
 {
--- tests/unit.tests/business_logic/DayPlanner/Time/TimeUnitTests.cpp
+++ tests/unit.tests/business_logic/DayPlanner/Time/TimeUnitTests.cpp
@@ -1,6 +1,6 @@
 #include "catch2/catch_test_macros.hpp"
 #include "Time.hpp"
-#include "fakeit.hpp"
+#include "../../../../../lib/FakeIt/single_header/catch/fakeit.hpp"
 
 TEST_CASE("Time can return current Hour")
 {
