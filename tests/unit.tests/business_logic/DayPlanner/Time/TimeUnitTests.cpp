9eaa3adbeb8f82479dc0d193fe84c6a64bfcf8bf
Jakub <jakubpasek11@gmail.com>
2024-10-29 17:53:42 +0100

    gettime

--- tests/unit.tests/business_logic/DayPlanner/Time/TimeUnitTests.cpp
+++ tests/unit.tests/business_logic/DayPlanner/Time/TimeUnitTests.cpp
@@ -10,10 +10,14 @@ using namespace dp_business_logic::DayPlanner;
 TEST_CASE("Time can return current Hour")
 {
     //Arrange
+
     std::unique_ptr<IDateTimeGetter> date_time_getter = std::make_unique<DateTimeGetter>();
     int utc_normalizator = 1;
+
+    auto excpected_result = std::chrono::duration_cast<std::chrono::hours>(std::chrono::system_clock::now().time_since_epoch()).count() % 24 + utc_normalizator;
+    auto result = date_time_getter -> GetCurrentHour() + 1;
     //Act
 
     //Assert
-    REQUIRE(17 == date_time_getter -> GetCurrentHour() + 1);
+    REQUIRE(result == excpected_result);
 }
