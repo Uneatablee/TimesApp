058dcbeb6dbcfd1d562d2d88f6188c366de0ec41
Jakub <jakubpasek11@gmail.com>
2025-03-23 14:59:22 +0100

    Quick IdException fix

--- src/business_logic/DayPlanner/include/IdFormatErrorException.hpp
+++ src/business_logic/DayPlanner/include/IdFormatErrorException.hpp
@@ -6,7 +6,7 @@ namespace dp_business_logic::DayPlanner
     class IdFormatErrorException final : public CustomDomainException
     {
     public:
-        IdFormatErrorException() : CustomDomainException(){}
+        IdFormatErrorException() : CustomDomainException("Base entity has invalid id form."){}
         IdFormatErrorException(const char* message) : CustomDomainException(message){}
 
     private:
